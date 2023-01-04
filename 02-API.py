dc7cf567c9ff7456d9e09ecc16671b9480774fab
EfreiAdrienTarcy <adrien.tarcy@efrei.net>
2023-01-04 11:09:37 +0100

    Changed second API file

--- 02-API.py
+++ 02-API.py
@@ -8,38 +8,30 @@ from flask import request, jsonify
 
 import joblib
 
-from prometheus_client import start_http_server, Summary,Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from prometheus_client import start_http_server, Summary,Counter, make_wsgi_app
+
+from prometheus_flask_exporter import PrometheusMetrics
 
 import random
 
 import time
-
-from multiprocessing.pool import ThreadPool
-
-
-c = Counter('my_failures', 'Description of counter')
-
-
-
-# Create a metric to track time spent and requests made.
-
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
 
 # Decorate function with metric.
 
 
 app = Flask(__name__)
 
+metrics = PrometheusMetrics(app)
+
 with open('knn.pkl', 'rb') as file:
 
     model = joblib.load(file)
 
 
-@REQUEST_TIME.time()
 
 @app.route('/predict', methods=['POST'])
-
 def get_predict():
 
   data = request.get_json(force=True)
@@ -53,23 +45,20 @@ def get_predict():
 
       df[col] = 0
 
-  c.inc()
-
   prediction = model.predict(df)
 
   return jsonify({'prediction': list(prediction)})
+
+response_data, content_type = metrics.generate_metrics()
+
 
 
 if __name__ == '__main__':
 
     # Start up the server to expose the metrics.
 
-    pool = ThreadPool(1)
-
-    pool.apply_async(start_http_server, (8000, ))
-
     model_columns = joblib.load('model_columns.pkl')
+    
+    app.run(debug=False)
 
-    app.run(port = 8081,debug=True)
-
-    #curl localhost:8081/predict -H "Content-Type: application/json" -d '{"sepal length (cm)": 5,"sepal width (cm)": 3,"petal length (cm)": 1,"petal width (cm)": 0.2}'
+    #curl localhost:5000/predict -H "Content-Type: application/json" -d '{"sepal length (cm)": 5,"sepal width (cm)": 3,"petal length (cm)": 1,"petal width (cm)": 0.2}'
